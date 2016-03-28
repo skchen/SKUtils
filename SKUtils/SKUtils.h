bf4adedb6645b8089190316322f73aa1a5d2f910
Shin-Kai Chen <skchen@sixnology.com>
2016-03-28 19:22:09 +0800

    Add iOS framework project

---
+++ SKUtils/SKUtils.h
@@ -0,0 +1,19 @@
+//
+//  SKUtils.h
+//  SKUtils
+//
+//  Created by Shin-Kai Chen on 2016/3/28.
+//  Copyright © 2016年 SK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKUtils.
+FOUNDATION_EXPORT double SKUtilsVersionNumber;
+
+//! Project version string for SKUtils.
+FOUNDATION_EXPORT const unsigned char SKUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKUtils/PublicHeader.h>
+
+
